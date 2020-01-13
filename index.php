de9cdc635b0f924784a7ed84cfee514929a0141c
RashiqulRony <rony.mmj@gmail.com>
2020-01-13 17:23:38 +0600

    New commit 1578914618

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('Git.php');
+
+    $repo = Git::open('D:\xampp\htdocs\gggg\gittest');
+
+    $branch = 'test'.time();
+    $repo->add('.');
+    $repo->commit('New commit '.time());
+    $repo->push('origin', 'master');
+
+    //    $repo->checkout($branch);
+    //    $repo->push('origin', $branch);
+
+    print_r($repo);exit();
+
