4e15a73af050ebc67eeec74a4058c6530718d0f4
RashiqulRony <rony.mmj@gmail.com>
2020-01-13 17:28:41 +0600

    New commit 1578914921

---
+++ croatian/number_lang.php
@@ -0,0 +1,10 @@
+<?php
+$lang['terabyte_abbr'] = 'TB';
+$lang['gigabyte_abbr'] = 'GB';
+$lang['megabyte_abbr'] = 'MB';
+$lang['kilobyte_abbr'] = 'kB';
+$lang['bytes'] = 'Baјtovi';
+
+
+/* End of file number_lang.php */
+/* Location: ./application/language/croatian/number_lang.php */
